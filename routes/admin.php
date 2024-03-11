1ec30f64d42a1645e958a3db6477198a07f98130
LilianBittar <lilian.bittar@pm.me>
2024-03-11 17:15:42 +0100

    Worked on Admin page with updating the info.

--- routes/admin.php
+++ routes/admin.php
@@ -2,5 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Backend\AdminController;
+use App\Http\Controllers\Backend\ProfileController;
 
 Route::get('dashboard', [AdminController::class, 'dashboard'])->name('dashboard');
+
+Route::get('profile', [ProfileController::class, 'index'])->name('profile');
+
+Route::post('profile/update', [ProfileController::class, 'updateProfile'])->name('profile.update');
